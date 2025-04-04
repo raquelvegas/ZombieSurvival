68773e2768d1c7b5f9974070e074f0d5dffa39d6
raquelvegas <raquel.vegas@edu.uah.es>
2025-04-04 11:31:58 +0200

    04-04 11:31

---
+++ src/main/java/zombieSurvival/Controller.java
@@ -0,0 +1,26 @@
+package zombieSurvival;
+
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+public class Controller {
+
+    @FXML
+    private Text
+            descansoText, comedorText, comidaCount, zonaComunText,
+
+            tunel1IzqText, tunel1MidText, tunel1DchText,
+            tunel2IzqText, tunel2MidText, tunel2DchText,
+            tunel3IzqText, tunel3MidText, tunel3DchText,
+            tunel4IzqText, tunel4MidText, tunel4DchText,
+
+            riesgo1IzqText, riesgo1DchText,
+            riesgo2IzqText, riesgo2DchText,
+            riesgo3IzqText, riesgo3DchText,
+            riesgo4IzqText, riesgo4DchText;
+
+
+
+
+
+}
