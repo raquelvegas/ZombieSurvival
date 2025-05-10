30553e581002f5d7427c6bcb5f1856bce71fc024
sergioruizj <sergio.ruizj@edu.uah.es>
2025-05-10 10:15:23 +0200

    Distribuida (III)

--- src/main/java/zombieSurvival/distribuida/MainCliente.java
+++ src/main/java/zombieSurvival/distribuida/MainCliente.java
@@ -1,9 +1,20 @@
 package zombieSurvival.distribuida;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
 public class MainCliente {
     public static void main(String[] args) {
-        while (true) {
-
+        try {
+            InterfazRMI informacion = (InterfazRMI) Naming.lookup("//127.0.0.1/Informacion");
+            for (int i = 0; i < 3; i++) {
+                Integer humanoRefugio = informacion.humanosEnRefugio();
+                System.out.println(humanoRefugio);
+            }
+        } catch (NotBoundException | MalformedURLException | RemoteException e) {
+            System.out.println("Error | Clase -> MainCliente | Método -> main | Excepcion en la localización del objeto distribuido");
         }
     }
 }
