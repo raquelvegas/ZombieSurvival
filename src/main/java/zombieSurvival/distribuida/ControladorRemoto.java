f1fecaa82b3a77718cf8d28fa4480b6285835dc1
raquelvegas <raquel.vegas@edu.uah.es>
2025-05-10 11:34:12 +0200

    10-05 11.34

---
+++ src/main/java/zombieSurvival/distribuida/ControladorRemoto.java
@@ -0,0 +1,14 @@
+package zombieSurvival.distribuida;
+
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+public class ControladorRemoto {
+    @FXML
+    private Text
+            hRefugio,
+            t1, t2, t3, t4,
+            hR1, hR2, hR3, hR4,
+            zR1, zR2, zR3, zR4,
+            muertes1, muertes2, muertes3;
+}
--- src/main/java/zombieSurvival/distribuida/MainCliente.java
+++ src/main/java/zombieSurvival/distribuida/MainCliente.java
@@ -1,12 +1,30 @@
 package zombieSurvival.distribuida;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 
-public class MainCliente {
-    public static void main(String[] args) {
+public class MainCliente extends Application {
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        // Crear la ventana principal (mainStage)
+        Stage remoteStage = new Stage();
+        FXMLLoader loader = new FXMLLoader(MainCliente.class.getResource("zombiesRemoto.fxml"));
+        Parent root = loader.load(); // Cargar el FXML
+        remoteStage.setTitle("Zombies Survival - Remoto");
+        remoteStage.setScene(new Scene(root));
+        remoteStage.setResizable(false);
+        remoteStage.show();
+
+
         try {
             InterfazRMI informacion = (InterfazRMI) Naming.lookup("//127.0.0.1/Informacion");
             for (int i = 0; i < 3; i++) {
